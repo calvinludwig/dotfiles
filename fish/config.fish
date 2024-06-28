5ca9f44be06605e53d7a692c2164eb4275dc4a0d
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-06-28 15:42:24 -0300

    feat: catppuccin delta

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,20 @@
-function fish_greeting 
+function fish_greeting
 end
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	source ~/Code/gpssat/documents/aliases.sh
-    
-	alias ls='exa -l --icons'
-	alias pn='pnpm'
-	alias vim='nvim -u NONE'
-	alias get_idf='. $HOME/.esp/esp-idf/export.fish'
+    source ~/Code/gpssat/documents/aliases.sh
 
-	zoxide init fish --cmd cd | source
+    alias ls='exa -l --icons'
+    alias pn='pnpm'
+    alias vim='nvim -u NONE'
+    alias get_idf='. $HOME/.esp/esp-idf/export.fish'
+    alias fzf="fzf --preview 'bat --color=always {}' --preview-window '~3'"
+    alias cat="bat -p"
+
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+    fzf --fish | source
+    zoxide init fish --cmd cd | source
     starship init fish | source
 end
 
@@ -19,7 +24,7 @@ source ~/.asdf/plugins/golang/set-env.fish
 # pnpm
 set -gx PNPM_HOME "/home/ludwig/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
