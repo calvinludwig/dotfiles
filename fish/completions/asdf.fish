d67773772d901c2b7a6f8cf0cf84391d590fbb68
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-06-28 12:11:36 -0300

    feat: allacrity and fish

---
+++ fish/completions/asdf.fish
@@ -0,0 +1,1 @@
+/home/ludwig/.asdf/completions/asdf.fish
---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+function fish_greeting 
+end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	source ~/Code/gpssat/documents/aliases.sh
+    
+	alias ls='exa -l --icons'
+	alias pn='pnpm'
+	alias vim='nvim -u NONE'
+	alias get_idf='. $HOME/.esp/esp-idf/export.fish'
+
+	zoxide init fish --cmd cd | source
+    starship init fish | source
+end
+
+source ~/.asdf/asdf.fish
+source ~/.asdf/plugins/golang/set-env.fish
+
+# pnpm
+set -gx PNPM_HOME "/home/ludwig/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+fish_add_path /home/ludwig/.spicetify
