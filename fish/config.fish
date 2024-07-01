678a1d205179e9156e02caf543e4a2e13c6e8db5
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-07-01 16:04:55 -0300

    idk

--- fish/config.fish
+++ fish/config.fish
@@ -6,14 +6,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/Code/gpssat/documents/aliases.sh
 
-
     alias g='git'
-    alias ls='exa -l --icons --group-directories-first'
+	alias ls='exa'
+    alias ll='exa --oneline --long --group --icons --git --header --group-directories-first'
     alias pn='pnpm'
     alias vim='nvim -u NONE'
     alias get_idf='. $HOME/.esp/esp-idf/export.fish'
     alias fzf="fzf --preview 'bat --color=always {}' --preview-window '~3'"
 	alias zj="zellij"
+
     alias cat="bat -p"
 
     export MANPAGER="sh -c 'col -bx | bat -l man -p'"
