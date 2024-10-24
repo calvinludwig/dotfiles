1890482ff5ed2ce5d153a56dcf207dd84623ee5a
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-10-24 11:30:37 -0300

    tokyoooooooooooooo

--- wezterm/lua/appearance.lua
+++ wezterm/lua/appearance.lua
@@ -3,44 +3,14 @@ local wezterm = require 'wezterm'
 
 local function scheme_for_appearance(appearance)
 	if appearance:find 'Dark' then
-		return 'Mocha'
+		return 'tokyonight_storm'
 	else
-		return 'WHITEppuccin'
+		return 'tokyonight_day'
 	end
-end
-
-local function mocha()
-	local custom = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
-	custom.tab_bar.background = '#0a0a0a'
-	custom.tab_bar.background = '#313244'
-	custom.tab_bar.inactive_tab.bg_color = '#313244'
-	custom.tab_bar.inactive_tab.fg_color = '#a6adc8'
-	custom.tab_bar.active_tab.bg_color = '#1e1e2e'
-	custom.tab_bar.active_tab.fg_color = '#74c7ec'
-	return custom
-end
-
-local function oledppuccin()
-	local custom = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
-	custom.background = '#000000'
-	custom.tab_bar.background = '#0a0a0a'
-	custom.tab_bar.inactive_tab.bg_color = '#141414'
-	return custom
-end
-
-local function whiteppuccin()
-	local custom = wezterm.color.get_builtin_schemes()['Catppuccin Latte']
-	custom.background = '#ffffff'
-	return custom
 end
 
 M.setup = function(c)
 	c.window_decorations = 'TITLE|RESIZE'
-	c.color_schemes = {
-		['Mocha'] = mocha(),
-		['OLEDppuccin'] = oledppuccin(),
-		['WHITEppuccin'] = whiteppuccin(),
-	}
 	c.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 	c.window_padding = {
 		left = '1cell',
--- wezterm/lua/fonts.lua
+++ wezterm/lua/fonts.lua
@@ -1,14 +1,14 @@
 local w = require 'wezterm'
 local M = {}
 
-local font_family = 'Maple Mono'
+local font_family = 'IBM Plex Mono'
 
 M.setup = function(c)
-	c.line_height = 1
-	c.font_size = 20
+	c.line_height = 1.1
+	c.font_size = 18
 	c.font = w.font_with_fallback {
 		{ family = font_family, weight = 'Regular' },
-		{ family = 'JetBrains Mono', weight = 'Regular' },
+		{ family = 'JetBrains Mono' },
 		{ family = 'Symbols Nerd Font', scale = 0.85 },
 		{ family = 'Noto Color Emoji', scale = 0.85 },
 	}
