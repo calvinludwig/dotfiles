03640bd68d90b44832941af8656cff3111d1accd
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-10-25 10:37:35 -0300

    tokyooooo storm

--- fish/config.fish
+++ fish/config.fish
@@ -6,32 +6,34 @@ if status is-interactive
     source ~/Code/gpssat/documents/aliases.sh
 
     alias g='git'
-	alias ls='exa'
+    alias ls='exa'
     alias ll='exa --oneline --long --group --icons --git --group-directories-first'
     alias pn='pnpm'
     alias vim='nvim -u NONE'
     alias get_idf='. $HOME/.esp/esp-idf/export.fish'
-	alias nv='nvim'
+    alias nv='nvim'
     alias fzf="fzf --preview 'bat --color=always {}' --preview-window '~3'"
-	alias inv='nvim $(fzf)'
-	alias zj="zellij"
-	alias sail="./vendor/bin/sail"
+    alias inv='nvim $(fzf)'
+    alias zj="zellij"
+    alias sail="./vendor/bin/sail"
 
     alias cat="bat -p"
 
-	fzf --fish | source
+    fzf --fish | source
 
     export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
+    eval (zellij setup --generate-auto-start fish | string collect)
     zoxide init fish | source
     starship init fish | source
 
-	source ~/.asdf/plugins/java/set-java-home.fish
 end
+
+source ~/.asdf/plugins/java/set-java-home.fish
 
 # pnpm
 set -gx PNPM_HOME "/home/ludwig/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   -- disable trouble
   { "folke/flash.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
