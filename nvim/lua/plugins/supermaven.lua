62e662350393428b89ab5cd397117f0cfdc2dcd9
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-11-07 12:52:27 -0300

    idk

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"supermaven-inc/supermaven-nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("supermaven-nvim").setup({
-				log_level = "off",
-			})
-		end,
-	},
+  {
+    "supermaven-inc/supermaven-nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("supermaven-nvim").setup({
+        log_level = "off",
+      })
+    end,
+  },
 }
