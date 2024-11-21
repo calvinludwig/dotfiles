51f1a1fc11bc43f2a0a63fe94432fe1e39799170
Calvin Ludwig <bludwig.calvin@gmail.com>
2024-11-21 17:21:47 -0300

    catppuccin all

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,3 +1,19 @@
+# Mocha
+# set -Ux FZF_DEFAULT_OPTS "\
+# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+# --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+# --color=selected-bg:#45475a \
+# --multi"
+
+# Macchiato
+# set -x FZF_DEFAULT_OPTS "\
+# --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+# --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+# --color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+# --color=selected-bg:#494d64 \
+# --multi"
+
 # Frappe
 # set -x FZF_DEFAULT_OPTS "\
 # --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
