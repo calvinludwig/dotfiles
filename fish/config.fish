b60f8dc90f5a5d9e4a6333e74c7ec125ed48231e
Calvin Ludwig <git@calvinludwig.xyz>
2024-12-30 08:43:37 -0300

    latte

--- fish/config.fish
+++ fish/config.fish
@@ -34,14 +34,14 @@ if status is-interactive
     starship init fish | source
 end
 
-set -x LS_COLORS $(vivid generate catppuccin-frappe)
-set -x EZA_COLORS $(vivid generate catppuccin-frappe)
+set -x LS_COLORS $(vivid generate catppuccin-latte)
+set -x EZA_COLORS $(vivid generate catppuccin-latte)
 
 set -x FZF_DEFAULT_OPTS "\
---color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
---color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
---color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
---color=selected-bg:#51576d \
+--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+--color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+--color=selected-bg:#bcc0cc \
 --multi"
 
 source ~/.asdf/plugins/java/set-java-home.fish
