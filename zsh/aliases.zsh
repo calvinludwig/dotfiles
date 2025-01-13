13110f6ec30bf5fa749ddcf8e3855c0a772bd9bb
Calvin Ludwig <git@calvinludwig.xyz>
2025-01-13 08:06:21 -0300

    some aliases and update lazyvim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,8 @@ alias sail="sh $([ -f sail ] && echo sail || echo vendor/bin/sail)"
 alias inv='nvim $(fzf --preview "bat --color=always {}" --preview-window "~3")'
 alias code="code --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto"
 alias cat="bat -p"
+
+alias phpstan="./vendor/bin/phpstan analyze"
+alias pint="./vendor/bin/pint"
+alias pest="./vendor/bin/pest"
+alias pestc="./vendor/bin/pest --coverage"
--- zsh/profile.zsh
+++ zsh/profile.zsh
@@ -1,1 +1,3 @@
 EDITOR=nvim
+
+. "$HOME/.cargo/env"
