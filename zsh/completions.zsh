528a6ad7d891052d63361707f55326042982fe03
Calvin Ludwig <git@calvinludwig.xyz>
2025-01-24 10:31:40 -0300

    mocha

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,2 +1,5 @@
 zinit light zsh-users/zsh-completions
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+# bun completions
+[ -s "/home/ludwig/.bun/_bun" ] && source "/home/ludwig/.bun/_bun"
--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -8,11 +8,34 @@ zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath' # 
 zstyle ':fzf-tab:*' use-fzf-default-opts yes # To make fzf-tab follow FZF_DEFAULT_OPTS.
 zstyle ':fzf-tab:*' switch-group '<' '>' # switch group using `<` and `>`
 
-export FZF_DEFAULT_OPTS=" \
---color=bg+:#ccd0da,spinner:#dc8a78,hl:#d20f39 \
---color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
---color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
---color=selected-bg:#bcc0cc \
+export FZF_LATTE=" \
+  --color=bg+:#ccd0da,spinner:#dc8a78,hl:#d20f39 \
+  --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+  --color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+  --color=selected-bg:#bcc0cc \
+  --multi"
+
+export FZF_FRAPPE=" \
+--color=bg+:#414559,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
+--color=selected-bg:#51576d \
 --multi"
+
+export FZF_MACCHIATO=" \
+--color=bg+:#363a4f,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+--color=selected-bg:#494d64 \
+--multi"
+
+export FZF_MOCHA=" \
+  --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+  --color=selected-bg:#45475a \
+  --multi"
+
+export FZF_DEFAULT_OPTS=$FZF_MOCHA
 
 eval "$(fzf --zsh)"
