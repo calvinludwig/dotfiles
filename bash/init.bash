e68db9ec95802e9b9cc1b73a86e52f39b1067f45
Calvin Ludwig <git@calvinludwig.xyz>
2025-09-02 14:07:01 -0300

    terraform

--- bash/init.bash
+++ bash/init.bash
@@ -2,6 +2,8 @@ source /usr/share/fzf/completion.bash
 source /usr/share/fzf/key-bindings.bash
 eval "$(symfony self:completion bash)"
 
+complete -C /usr/bin/terraform terraform
+
 eval "$(fzf --bash)"
 eval "$(mise activate bash)"
 eval "$(zoxide init bash)"
