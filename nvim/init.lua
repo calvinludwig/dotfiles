6b4dbde79d3bacc395f62b0cec5fb9fca5038fec
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2025-10-30 18:50:15 -0300

    removing catppuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,8 @@
 require 'options'
 
 -- vim.pack.add {
---   { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
 --   { src = 'https://github.com/nvim-treesitter/nvim-treesitter', branch = 'master', build = ':TSUpdate' },
 -- }
---
--- vim.cmd.colorscheme 'catppuccin-latte'
 --
 -- require('nvim-treesitter.configs').setup {
 --   highlight = {
