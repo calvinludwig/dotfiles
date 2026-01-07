0c3f05f9265640beb9362a2fc846d4406e6cc4d2
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2026-01-07 16:15:09 -0300

    idk

---
+++ fish/config.fish
@@ -0,0 +1,30 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+export EDITOR=nvim
+export VISUAL=nvim
+export MICRO_TRUECOLOR=1
+
+set -gx UID 1000
+set -gx GID 1000
+
+if status is-interactive
+
+    fish_config theme choose "Catppuccin Mocha"
+
+    alias ff="fastfetch"
+    alias nv='nvim'
+    alias inv='nvim $(fzf --preview "bat --color=always {}" --preview-window "~3")'
+    alias cat="bat -p"
+
+    alias ..="z .."
+    alias ...="z ../.."
+
+    alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
+
+    zoxide init fish | source
+    starship init fish | source
+end
+
+function fish_greeting
+    fastfetch
+end
