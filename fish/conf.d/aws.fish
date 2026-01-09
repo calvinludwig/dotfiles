556ea40a401616316a3768c5f16543477d7f0684
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2026-01-09 20:38:50 -0300

    aws list ec2

--- fish/conf.d/aws.fish
+++
@@ -1,5 +0,0 @@
-function aws-ec2-list
-    aws ec2 describe-instances \
-        --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value|[0],InstanceId,State.Name,InstanceType,PublicIpAddress,PrivateIpAddress,LaunchTime]' \
-        --output table
-end
