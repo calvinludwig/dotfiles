3dfbd1e425f3df7c9c7b75b70088a1416c8a5e4f
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2026-01-14 17:45:03 -0300

    lazyvim config

--- .config/nvim/lua/plugins/php.lua
+++ .config/nvim/lua/plugins/php.lua
@@ -2,10 +2,5 @@ return {
   -- 'calvinludwig/mago.nvim',
   dir = '/home/me/code/personal/mago.nvim',
   ft = 'php',
-  opts = {
-    lint_on_save = true,
-    config = function()
-      require('lspconfig').intelephense.setup {}
-    end,
-  },
+  opts = {},
 }
