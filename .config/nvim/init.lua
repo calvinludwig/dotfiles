39da9b4ccc60d234668372e3f7af4702a3d66ecd
Calvin Ludwig <31071765+calvinludwig@users.noreply.github.com>
2026-01-15 18:28:36 -0300

    nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,12 +23,11 @@ vim.opt.incsearch = true -- Show matches as you type
 -- Visual settings
 vim.opt.termguicolors = true -- Enable 24-bit colors
 vim.opt.signcolumn = 'yes' -- Always show sign column
-vim.opt.colorcolumn = '100' -- Show column at 100 characters
 vim.opt.showmatch = true -- Highlight matching brackets
 vim.opt.matchtime = 2 -- How long to show matching bracket
 vim.opt.cmdheight = 1 -- Command line height
 vim.opt.completeopt = 'menuone,noinsert,noselect' -- Completion options
-vim.opt.showmode = false -- Don't show mode in command line
+vim.opt.showmode = true --false -- Don't show mode in command line
 vim.opt.pumheight = 10 -- Popup menu height
 vim.opt.pumblend = 10 -- Popup menu transparency
 vim.opt.winblend = 0 -- Floating window transparency
@@ -36,6 +35,7 @@ vim.opt.conceallevel = 0 -- Don't hide markup
 vim.opt.concealcursor = '' -- Don't hide cursor line markup
 vim.opt.lazyredraw = true -- Don't redraw during macros
 vim.opt.synmaxcol = 300 -- Syntax highlighting limit
+vim.opt.winborder = 'bold'
 
 -- File handling
 vim.opt.backup = false -- Don't create backup files
@@ -70,19 +70,67 @@ vim.opt.foldlevel = 99 -- Start with all folds open
 -- Split behavior
 vim.opt.splitbelow = true -- Horizontal splits go below
 vim.opt.splitright = true -- Vertical splits go right
+
+-- Key mappings
+vim.g.mapleader = ' ' -- Set leader key to space
+vim.g.maplocalleader = ' ' -- Set local leader key (NEW)
+
+-- Normal mode mappings
+vim.keymap.set('n', '<leader>c', ':nohlsearch<CR>', { desc = 'Clear search highlights' })
+
+-- Center screen when jumping
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result (centered)' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result (centered)' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Half page down (centered)' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Half page up (centered)' })
+
+-- Buffer navigation
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = 'Previous buffer' })
+
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to bottom window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to top window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
+-- Splitting & Resizing
+vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>sh', ':split<CR>', { desc = 'Split window horizontally' })
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height' })
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase window width' })
+
+-- Move lines up/down
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+-- Better indenting in visual mode
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
+
+-- Better J behavior
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines and keep cursor position' })
+
+-- Plugins
 vim.pack.add {
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/rose-pine/neovim' },
+  { src = 'https://github.com/calvinludwig/mago.nvim' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
   {
-    name = 'rose-pine',
-    src = 'https://github.com/rose-pine/neovim',
+    src = 'https://github.com/saghen/blink.cmp',
+    version = vim.version.range '*',
   },
-  {
-    name = 'mago.nvim',
-    src = 'https://github.com/calvinludwig/mago.nvim',
-  },
-  {
-    name = 'nvim-treesitter',
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-  },
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+  { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim' },
+  { src = 'https://github.com/nvim-telescope/telescope-ui-select.nvim' },
+  { src = 'https://github.com/stevearc/oil.nvim' },
+  { src = 'https://github.com/chomosuke/typst-preview.nvim' },
 }
 
 vim.cmd 'colorscheme rose-pine-dawn'
@@ -90,9 +138,18 @@ vim.cmd 'colorscheme rose-pine-dawn'
 require('mago-nvim').setup()
 
 vim.lsp.enable {
-  'lua_ls',
+  'copilot',
+  'clangd',
   'gopls',
+  'lua_ls',
   'jsonls',
+  'yamlls',
+  'rust_analyzer',
+  'docker-language-server',
+  'html',
+  'typst_lsp',
+  'taplo',
+  'sqls',
 }
 
 vim.diagnostic.config {
@@ -118,3 +175,131 @@ vim.diagnostic.config {
     },
   },
 }
+
+require('blink.cmp').setup {
+  keymap = { preset = 'default' },
+  appearance = {
+    use_nvim_cmp_as_default = true,
+    nerd_font_variant = 'mono',
+  },
+  signature = { enabled = true },
+}
+
+require('oil').setup {
+  view_options = {
+    show_hidden = true,
+  },
+  lsp_file_methods = {
+    enabled = true,
+    timeout_ms = 1000,
+    autosave_changes = true,
+  },
+  columns = {
+    'icon',
+  },
+  float = {
+    max_width = 0.5,
+    max_height = 0.7,
+    border = 'rounded',
+  },
+}
+
+require('telescope').setup {
+  defaults = {
+    preview = { treesitter = true },
+    color_devicons = true,
+    sorting_strategy = 'ascending',
+    path_displays = { 'smart' },
+    layout_config = {
+      height = 100,
+      width = 400,
+      prompt_position = 'top',
+      preview_cutoff = 40,
+    },
+  },
+  extensions = {
+    ['ui-select'] = {
+      require('telescope.themes').get_dropdown {
+        -- even more opts
+      },
+    },
+    fzf = {},
+  },
+}
+require('telescope').load_extension 'ui-select'
+require('telescope').load_extension 'fzf'
+
+vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Code Format' })
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Code Rename' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Actions' })
+
+-- telescope
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- oil
+vim.keymap.set('n', '<leader>e', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
+
+-- highlight yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+  pattern = '*',
+  desc = 'highlight selection on yank',
+  callback = function()
+    vim.highlight.on_yank { timeout = 200, visual = true }
+  end,
+})
+
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, mark)
+      -- defer centering slightly so it's applied after render
+      vim.schedule(function()
+        vim.cmd 'normal! zz'
+      end)
+    end
+  end,
+})
+
+-- open help in vertical split
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
+  command = 'wincmd L',
+})
+
+-- auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd('VimResized', {
+  command = 'wincmd =',
+})
+
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd('BufRead', {
+  group = vim.api.nvim_create_augroup('dotenv_ft', { clear = true }),
+  pattern = { '.env', '.env.*' },
+  callback = function()
+    vim.bo.filetype = 'dosini'
+  end,
+})
+
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
+  group = vim.api.nvim_create_augroup('active_cursorline', { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+-- show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ 'WinLeave', 'BufLeave' }, {
+  group = 'active_cursorline',
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
